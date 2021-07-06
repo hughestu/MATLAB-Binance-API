8ea2ae41d975eac278eda8da7df56213f596dad4
hughestu <hughestu@tcd.ie>
2021-07-06 22:56:02 +0100

    getkeys_Template

--- subfunctions/getkeys_Template.m
+++ subfunctions/getkeys_Template.m
@@ -1,18 +1,16 @@
 function [akey,skey] = getkeys_Template(accountName)
-% Use this make the getkeys function.
+% Use this as a template
 %
 % Instructions:
 %
 % (1) Assign the public and private keys to akey and skey, 
 % respectively for one or more accounts. Modify account names as required.
 %
-% (2) The account name listed in the arguments block will be the account 
-% for all calls to Binance. List the account you wish to use by default in
-% the arguments block, ensure the name of the default account (in this 
-% example the account itself is called 'default') matches one of the
-% account names in the switch block
+% (2) List the account you wish to use by default in the arguments block.
+% Currently, the default account is called 'default'. Ensure the name of 
+% the default account matches one of the account names in the switch block.
 %
-% (3) Finally, rename the function from getkeys_Template.m to getkeys.m
+% (3) Lastly, rename the function from getkeys_Template.m to getkeys.m
 %
 % You can test that the keys are correct by calling spot.accountInfo
 
@@ -37,4 +35,4 @@ switch accountName
 end
 
 assert(~or(isempty(akey),isempty(skey)), ['This api request requires '...
-    ' public and private keys. Refer to guidelines on Github'])
+    ' public and private keys.'])
