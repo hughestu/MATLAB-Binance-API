3c496383699e6d10f4a2c4a6d5ca931d956b3c2f
hughestu <hughestu@tcd.ie>
2021-07-12 10:45:39 +0100

    More doc for pub.historicalTrades and changed pub.historicalTrades s.t. fromId is now a positional argument rather than name-value pair argument (in-line with previous change to pub.aggTrades).
    Added example 2 into pub.bookDepth help doc

--- +pub/bookDepth.m
+++ +pub/bookDepth.m
@@ -4,12 +4,21 @@ function T = bookDepth(symbol,OPT)
 % T = pub.bookDepth(symbol) returns the orderbook for a specific symbol.
 % Input symbol is a type char row vector.
 %
-% T = pub.bookDepth(symbol,'limit',val) returns up to val rows of bid/ask
+% T = pub.bookDepth(symbol,'limit',n) returns up to n rows of bid/ask
 % prices and quantities for a specific symbol (default limit: 100).
 %  - Valid inputs for the limit value: 5, 10, 20, 50, 100, 500, 1000, 5000.
 %
-% Example:
+% Example 1 (get 100 rows from the current btcusdt orderbook):
+%  >> T = pub.bookDepth('btcusdt');
+%
+% Example 2 (get 5000 rows from the BTC/USDT orderbook and then plot the 
+% spread):
 %  >> T = pub.bookDepth('btcusdt','limit',5000);
+%     figure(), plot(T.bidPrice,cumsum(T.bidQty),'g','LineWidth',2)
+%     hold on, plot(T.askPrice,cumsum(T.askQty),'r','LineWidth',2)
+%     ylabel('Quantity, Q / (BTC)')
+%     xlabel('Price, P / (USDT)')
+%     set(gca,'fontSize',16)
 
 arguments
     symbol      (1,:) char
@@ -27,19 +36,12 @@ QP = matlab.net.QueryParameter(OPT);
 
 s = webread([getBaseURL '/api/v3/depth?' QP.char]);
 
-% convert to table
+% Convert to table
 C = cellfun(@(x,y) [str2double(x).' str2double(y).'],s.bids,s.asks,...
     'uni',false);
 
 T = array2table(cell2mat(C),...
     'VariableNames',{'bidPrice','bidQty','askPrice','askQty'});
 end
-%{
-T = pub.bookDepth('btcusdt','limit',5000);
-figure(), plot(T.bidPrice,cumsum(T.bidQty),'g','LineWidth',2)
-hold on, plot(T.askPrice,cumsum(T.askQty),'r','LineWidth',2)
-ylabel('Quantity, Q / (BTC)')
-xlabel('Price, P / (USDT)')
-set(gca,'fontSize',16)
-%}
+
 
--- +pub/historicalTrades.m
+++ +pub/historicalTrades.m
@@ -1,29 +1,91 @@
-function [T,s] = historicalTrades(symbol,OPT)
-% historicalTrades returns older market trades (up to 1000 per request).
+function [T,s] = historicalTrades(varargin,OPT)
+% historicalTrades returns market trade history for a specific symbol.
 %  
-% T = pub.historicalTrades(symbol) returns the last 500 trades in a table
-% giving the tradefor the
-% symbol, where symbol is a type char row vector.
+% T = pub.historicalTrades(symbol) returns a 500 row timetable of the
+% latest trades for a specific symbol (where symbol is a type char row 
+% vector).
+% e.g. >> T = pub.historicalTrades('btcusdt')
 %
-% T = pub.historicalTrades(___,'limit',val) returns up to val historical
-% trades (val is valid in the range 1 <= val <= 1000).
+% T = pub.historicalTrades(symbol,fromId) returns trades with trade id >= 
+% fromId for a specific symbol (the first trade of a symbol's trade history
+% has a trade id of 1 and subsequent trade id's are numbered consecutively 
+% thereafter).
 %
-% T = pub.historicalTrades(___,'fromId',tradeId) returns trades subsequent 
-% to the input tradeId, where the first trade on a symbol has tradeId = 1
-% and subsequent tradeId's increase consecutively by 1.
+% T = pub.historicalTrades(___,'limit',n) returns n rows of historical
+% trade data where n is valid in the range 1-1000 (default: n = 500).
 %
-% [T,s] = pub.historicalTrades(___) returns the unformatted server response
-% as a structure, s.
+% [T,s] = pub.historicalTrades(___) returns the orignal server response
+% data as a structure, s.
+%
+%
+% Example 1 (get the first 100 trades recorded on Binance for BNB/BTC):
+%  >> T = pub.historicalTrades('bnbbtc',1,'limit',100)
+%
+%   - remark: In trade number 15 and 20, a total of 14000 BNB sold for 
+%             0.14 BTC or an equivalent of $320 at the time. In todays 
+%             market (3 years later) 14000 BNB is worth approx. $4,500,000.
+%
+%
+% Example 2 (get the latest 1000 trades for BTC/USDT):
+%  >> T = pub.historicalTrades('btcusdt','limit',1000)
+% 
+%
+% Example 3 (scan forwards through trade history for btcusdt, starting from 
+% a specific trade id, and store results in a single timetable, TT):
+%  >> id = 9.5e8 + 1;     % nominal trade id to start from
+%     N = 5;              % loop iterations
+%     n = 1000;           % number of trades to receive per api request
+%
+%     % Preallocate a timetable
+%     T = pub.historicalTrades('btcusdt','limit',1);
+%     TT = repmat(T,N*n,1); 
+% 
+%     for ii = 1:N
+%         T = pub.historicalTrades('btcusdt',id,'limit',n);
+%         TT(1+(ii-1)*n:n*ii,:) = T;
+%         T.time(1+(ii-1)*n:n*ii,:) = T.time;
+%         id = id + n;
+%     end
+%
+%
+% Example 4 (scan backwards through trade history for btcusdt, and store
+% results in a single timetable, TT):
+%  >> N = 5;              % loop iterations
+%     n = 1000;           % number of trades to receive per api request
+% 
+%     % Preallocate a timetable
+%     T = pub.historicalTrades('btcusdt','limit',1);
+%     TT = repmat(T,N*n,1); 
+%
+%     id = T.id;          % most recent trade id
+%
+%     for ii = 1:N
+%         id = id - n;
+%         T = pub.historicalTrades('btcusdt',id,'limit',n);
+%         TT(1+n*(5-ii):n*(6-ii),:) = T;
+%         TT.time(1+n*(5-ii):n*(6-ii),:) = T.time;
+%     end
 
+arguments (Repeating)
+    varargin
+end
 
-arguments
-    symbol      (1,:) char      
+arguments     
     OPT.limit   (1,1) double    = 500
-    OPT.fromId 	(1,1) double
+end
+
+assert(nargin>=1 && nargin <=2,sprintf(...
+    'Expected 1-2 positional input arguments, but there were %d.',nargin))
+
+validateattributes(varargin{1},{'char'},{'row'})
+OPT.symbol = upper(varargin{1});
+
+if nargin == 2
+    validateattributes(varargin{2},{'double'},{'scalar'})
+    OPT.fromId = sprintf('%d',varargin{2});
 end
 
 import matlab.net.*
-OPT.symbol = upper(symbol);
 
 akey =  getkeys();
 
@@ -45,10 +107,6 @@ T.price = str2double(T.price);
 T.qty = str2double(T.qty);
 T.quoteQty = str2double(T.quoteQty);
 
-time = T.time;
-T.time = [];
+T.time = datetime(T.time*1e-3,'ConvertF','posixtime','TimeZone','local');
 
-T = table2timetable(T,'RowTimes',...
-    datetime(time*1e-3,'ConvertF','posixtime','TimeZone','local'));
-
-
+T = table2timetable(T,'RowTimes','time');
