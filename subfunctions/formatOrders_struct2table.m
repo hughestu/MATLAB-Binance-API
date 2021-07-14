a8a1186d7e95b36b9cd2abdb16995a136aa4952e
hughestu <hughestu@tcd.ie>
2021-07-14 18:17:40 +0100

    I added in the case for a scalar structure (which previously threw an error).

--- subfunctions/formatOrders_struct2table.m
+++ subfunctions/formatOrders_struct2table.m
@@ -1,11 +1,21 @@
 function t = formatOrders_struct2table(s)
 t = struct2table(s);
 fnames = fieldnames(s);
-for c = [5:8 13 14 18]
-    t.(fnames{c}) = cellfun(@str2double,t.(fnames{c}));
-end
-for c = [1 4 9:12]
-    t.(fnames{c}) = cellfun(@string,t.(fnames{c}));
+
+if height(t) > 1
+    for c = [5:8 13 14 18]
+        t.(fnames{c}) = cellfun(@str2double,t.(fnames{c}));
+    end
+    for c = [1 4 9:12]
+        t.(fnames{c}) = cellfun(@string,t.(fnames{c}));
+    end
+else
+    for c = [5:8 13 14 18]
+        t.(fnames{c}) = cellfun(@str2double,{t.(fnames{c})});
+    end
+    for c = [1 4 9:12]
+        t.(fnames{c}) = cellfun(@string,{t.(fnames{c})});
+    end
 end
 t.time = datetime(t.time./1000,'ConvertFrom','posixtime');
 t.updateTime = datetime(t.updateTime./1000,'ConvertFrom','posixtime');
