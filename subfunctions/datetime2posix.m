d96f6f7a2ac6e8374e6132ce840f3e5a15ede285
hughestu <hughestu@tcd.ie>
2021-08-28 11:57:27 +0100

    Applies local timezone if the timezone field is empty

--- subfunctions/datetime2posix.m
+++ subfunctions/datetime2posix.m
@@ -1,9 +1,11 @@
 function out = datetime2posix(varargin)
 % t_posix = datetime2posix(t) takes a datetime array, t, and converts to 
-% posixtime in milliseconds (conversion uses the users local timezone).
+% posixtime in milliseconds.
 %
 % OPT = datetime2posix(OPT,param) applies the conversion to a paramter, 
 % param, in the structure, OPT.
+%
+% datetimes defined without a timezone use the 'local' timezone by default.
 
 assert(nargin<=2,sprintf('Expected 1 - 2 input arguments, not %d.',nargin))
 
@@ -11,7 +13,10 @@ if nargin == 1
     
     t = varargin{1};
     assert(isa(t,'datetime'),'Expected single input to be a datetime')
-    t.TimeZone = 'local';
+    
+    if isempty(t.TimeZone)
+        t.TimeZone = 'local';
+    end
     out = posixtime(t)*10^3;
     
 else
@@ -22,7 +27,10 @@ else
     assert(isfield(OPT,param), sprintf(...
         'The input structure has no fieldnames called %s.',param))
     
-    OPT.(param).TimeZone = 'local';
+    if isempty(OPT.(param).TimeZone)
+        OPT.(param).TimeZone = 'local';
+    end
+    
     OPT.(param) = posixtime(OPT.(param))*10^3;
     
     out = OPT;
